2925a052f0298bfff7bf1d0fd81189037619df60
RadityaPratama03 <RadityaPratama03>
2021-06-26 03:17:49 +0700

    Activity 10 dan 11

---
+++ app/src/main/java/com/example/firebasebarang/Barang.java
@@ -0,0 +1,43 @@
+package com.example.firebasebarang;
+
+import java.io.Serializable;
+
+public class Barang implements Serializable {
+    private String idfb;
+    private String nama;
+    private String kode;
+
+    public Barang(){
+    }
+
+    public String getKode(){
+        return kode;
+    }
+    public void setKode(String kd){
+        this.kode = kd;
+    }
+
+    public String getNama(){
+        return nama;
+    }
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getId(){return idfb;}
+    public void setId(String id){this.idfb = id;}
+
+    @Override
+    public String toString() {
+
+        return "Barang{" +
+                ", nama='" + nama + '\n'+
+                ", kode='" + kode + '\n' +
+                "id='" + idfb + '\n' +
+                '}';
+    }
+    public Barang(String kd, String nm){
+        kode = kd;
+        nama = nm;
+    }
+}
---
+++ app/src/main/java/com/example/firebasebarang/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.firebasebarang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.firebasebarang.LihatBarang;
+import com.example.firebasebarang.R;
+import com.example.firebasebarang.TambahData;
+
+public class MainActivity extends AppCompatActivity {
+    private Button bTambah;
+    private Button bLihat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bTambah = findViewById(R.id.btTambah);
+        bLihat = findViewById(R.id.btLihat);
+
+        bTambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, TambahData.class);
+                startActivity(i);
+//                startActivity(TambahData.getActIntent(MainActivity.this));
+            }
+        });
+        bLihat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(LihatBarang.getActIntent(MainActivity.this));
+            }
+        });
+    }
+}
